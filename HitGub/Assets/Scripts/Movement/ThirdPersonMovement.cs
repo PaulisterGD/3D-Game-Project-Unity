61a56e9e21972ae01539fec30ab89f3bd071dcfa
PaulisterGD <paullambropoulos93@gmail.com>
2022-04-08 17:09:42 +0200

    Putting comments into code
    For portfolio evidence

--- HitGub/Assets/Scripts/Movement/ThirdPersonMovement.cs
+++ HitGub/Assets/Scripts/Movement/ThirdPersonMovement.cs
@@ -4,44 +4,47 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    //PUBLIC DECLARATIONS - CharacterController, Camera and an invisible object that checks when the player is on 
     public CharacterController controller;
     public Transform cam;
-    //public Camera interactCam;
     public Transform groundCheck;
 
+    //PUBLIC DECLARATIONS - SPEED VARIABLES
     public float speed = 6f;
     public float gravity = -9.81f;
     Vector3 velocity;
 
+    //PUBLIC DECLARATIONS - GROUND CHECKER
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
 
+    //VARIABLES - CAMERA TURNING VARIABLES
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
-    //public Interactable focus;
 
     // Update is called once per frame
     void Update()
     {
+        //Checker on whether the player is touching the ground, sets the velocity to a fixed value when touching the ground.
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0) { velocity.y = -1f; }
 
-        if (isGrounded && velocity.y < 0) {
-            velocity.y = -1f;
-        }
-
+        //Receiving the axes from the Character Controller component
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        //Setting the movement of the player model
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        //Changing the gravity of the player model
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
+        //Check on whether the player model is actually moving, then change the angle of rotation of the model to face the direction of motion
         if (direction.magnitude >= 0.1f) { 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);      
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
