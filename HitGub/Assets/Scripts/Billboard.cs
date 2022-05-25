6360072b5d63b2fae92cf5893ffb954c8c2c64cf
mawitka <97891077+mawitka@users.noreply.github.com>
2022-05-25 11:43:14 +0200

    UI SAMPLE

---
+++ HitGub/Assets/Scripts/Billboard.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera theCam;
+
+    public bool useStaticBillboard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;   //asigning the camera
+    }
+
+    // positioning the 2d sprite to be facing the camera from every angle
+    void LateUpdate()
+    {
+        if(!useStaticBillboard)
+        {
+            transform.LookAt(theCam.transform);
+        } else
+        {
+            transform.rotation = theCam.transform.rotation;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    }
+}
