e46c3c435b312b1cde8c0a771f80b222288e0cf1
mawitka <97891077+mawitka@users.noreply.github.com>
2022-06-13 10:29:43 +0200

    zoom
    zoom working

--- HitGub/Assets/Scripts/ButtonBehaviour.cs
+++ HitGub/Assets/Scripts/ButtonBehaviour.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class ButtonBehaviour : MonoBehaviour
 {
+    public CinemachineFreeLook vcam;
+    public bool zoomCheck;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void Zoom()
+    {
+        if (zoomCheck == true)
+        {
+            vcam.m_CommonLens = true;
+            vcam.m_Lens.FieldOfView = 10;
+
+            zoomCheck = false;
+        }
+        else
+        {
+            vcam.m_CommonLens = true;
+            vcam.m_Lens.FieldOfView = 40;
+            zoomCheck = true;
+        }
+
+    }
+
 
     public void SwitchUI(string nameUI)
 	{
